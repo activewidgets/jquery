29e7e64e145ab58f80315979e40ec77e6fdfb21e
alex <alex@activewidgets.com>
2020-01-19 11:05:31 +0100

    examples

--- js/framework.js
+++ js/framework.js
@@ -4,5 +4,5 @@ import $ from 'jquery';
 import * as preact from 'preact';
 import htm from 'htm';
 
-export const {h, build} = adapter($, preact);
+export const {h, build} = adapter(preact, $);
 export const tpl = htm.bind(h);
