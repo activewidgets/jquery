f46698b05696cbe190524d8666bfd169c8830473
alex <alex@activewidgets.com>
2020-01-19 12:38:56 +0100

    do not export component classes

--- js/datagrid.js
+++ js/datagrid.js
@@ -3,4 +3,4 @@ import {build} from './framework.js';
 import * as components from '@activewidgets/datagrid/js';
 import * as metadata from '@activewidgets/datagrid/metadata';
 
-export const {Datagrid, Row, Gridview, Scrollbars} = build(components, metadata);
+build(components, metadata);
