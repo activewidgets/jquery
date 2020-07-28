51b60a01808636eae282014b0a31814c92e48553
alex <alex@activewidgets.com>
2020-07-28 18:14:26 +0200

    allow tree-shaking

--- js/framework.js
+++ js/framework.js
@@ -6,8 +6,8 @@
 
 import adapter from '@activewidgets/frameworks/jquery';
 import $ from 'jquery';
-import * as preact from 'preact';
+import {Component, cloneElement, createElement, isValidElement, render} from 'preact';
 import htm from 'htm';
 
-export const {h, build} = adapter(preact, $);
+export const {h, build} = adapter({Component, cloneElement, createElement, isValidElement, render}, $);
 export const tpl = htm.bind(h);
