01c776bac446563c98a13d2f62fb720a900f6ae6
alex <alex@activewidgets.com>
2021-02-08 00:47:27 +0100

    update testing

--- rollup.config.js
+++ rollup.config.js
@@ -56,6 +56,7 @@ let plugins = [
     resolve(),
     babel({
         babelrc: false,
+        babelHelpers: 'bundled',
         exclude: 'node_modules/**',
         presets: [["@babel/env", {modules: false}]]
     }),
