11e86cc3cdf02ad183b2b3f0773b5e7294b9441f
alex <alex@activewidgets.com>
2022-01-24 13:46:59 +0100

    copyright year

--- rollup.config.js
+++ rollup.config.js
@@ -34,7 +34,7 @@ let roots = {
 
 let getBanner = name => `/**
  * ${name} ${rootpkg.version}
- * Copyright (C) 2020 ActiveWidgets SARL. All Rights Reserved.
+ * Copyright (C) 2022 ActiveWidgets SARL. All Rights Reserved.
  * This code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this package.
  */
@@ -43,7 +43,7 @@ let getBanner = name => `/**
 
 function keepBanner(node, comment){
     if (comment.type == "comment2") {
-        return /\(C\) 2020 ActiveWidgets/.test(comment.value);
+        return /\(C\) 2022 ActiveWidgets/.test(comment.value);
     }
 }
 
@@ -96,7 +96,7 @@ let modules = roots.modules.map(dir => {
 
 let getCommercial = () => `/**
  * ${rootpkg.name} ${rootpkg.version}
- * Copyright (C) 2020 ActiveWidgets SARL. All Rights Reserved.
+ * Copyright (C) 2022 ActiveWidgets SARL. All Rights Reserved.
  * This code is licensed under the COMMERCIAL license.
  */
 `;
