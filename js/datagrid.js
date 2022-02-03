f5173ae239808555712f6cac0d48b77c5d80d6ce
alex <alex@activewidgets.com>
2022-02-03 21:15:02 +0100

    merge metadata into component

--- js/datagrid.js
+++ js/datagrid.js
@@ -6,6 +6,5 @@
 
 import {build} from './framework.js';
 import * as components from '@activewidgets/datagrid/js';
-import * as metadata from '@activewidgets/datagrid/metadata';
 
-build(components, metadata);
+build(components);
--- rollup.config.js
+++ rollup.config.js
@@ -20,7 +20,6 @@ let globals = {
     '@activewidgets/frameworks/jquery': 'ActiveWidgets.Frameworks.jQuery',
     '@activewidgets/datagrid/js': 'ActiveWidgets.Components',
     '@activewidgets/datagrid/style': 'ActiveWidgets.Styles',
-    '@activewidgets/datagrid/metadata': 'ActiveWidgets.Metadata',
     '@activewidgets/datagrid/dist/datagrid.css': '',
     '@activewidgets/datagrid/css': ''
 };
