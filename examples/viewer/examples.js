9ab5444f7fb758592123db5cc9d23661bb3cdd07
alex <alex@activewidgets.com>
2022-03-08 11:12:25 +0100

    remove cdn-es5 example

--- examples/viewer/examples.js
+++ examples/viewer/examples.js
@@ -5,14 +5,12 @@
  */
 
 import hello_world from '../hello-world/README.md';
-import cdn_es5 from '../cdn-es5/README.md';
 import columns from '../columns/README.md';
 import events from '../events/README.md';
 
 
 export const Local = {
     'Hello, World!': {path: 'hello-world', readme: hello_world},
-    'CDN (no build)': {path: 'cdn-es5', readme: cdn_es5},
     'Columns, rows': {path: 'columns', readme: columns},
     'User events': {path: 'events', readme: events}
 };
